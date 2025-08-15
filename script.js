0e30f5bb1cdb6767d524189113ab0aefaca8a47d
Kaiobelphman <161335847+Kaiobelphman@users.noreply.github.com>
2025-08-15 10:58:24 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Você conhece Dragon ball? Aqui vai algumas pergunatas!",
         alternativas: [
             {
-                texto: "Isso é assustador!",
+                texto: "Bora!",
                 afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
             },
             {
-                texto: "Isso é maravilhoso!",
+                texto: "Claro!!",
                 afirmacao: "Quis saber como usar IA no seu dia a dia."
             }
         ]
